bc00c20248074f4d3639316c1c297ef0caa28697
Shae Erisson <shae@scannedinavian.com>
2022-05-08 15:09:50 -0400

    add a demo secrets.py

---
+++ Workshop_2022/secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : 'Mass Distraction_2G',             # Keep the two '' quotes around the name
+    'password' : 'thepassword',         # Keep the two '' quotes around password
+    'timezone' : "America/New_York",  # http://worldtimeapi.org/timezones
+    'rc_calendar_token' : 'randomlettersandnumbers',
+}
